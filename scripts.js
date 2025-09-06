b3a832e2246e419fd9a74ea8e8a64b06a9df4392
Jordan <jordan@kalosal.com>
2025-09-06 10:10:31 -0500

    Dont show scenario until they click start conversation

--- scripts.js
+++ scripts.js
@@ -16,9 +16,17 @@ document.addEventListener("DOMContentLoaded", (event) => {
 function startConversation() {
   const button = document.querySelector(".cta-button");
   const iframe = document.querySelector(".chat-container");
+  const scenario = document.querySelector(".scenario-description");
+  const topics = document.querySelector(".topics");
 
   if (button && iframe) {
     button.style.display = "none";
     iframe.style.display = "block";
+  }
+  if (scenario) {
+    scenario.style.display = "block";
+  }
+  if (topics) {
+    topics.style.display = "block";
   }
 }
